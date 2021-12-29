c1a49c66e554245e9d706d31ee6fc743cc77f819
valentinopagani <paganivalentinosk@gmail.com>
2021-12-28 21:15:42 -0300

    master

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Characters from "./components/Characters";
+import Pagination from "./components/Pagination";
+import logo from "./assets/logo.png";
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+  const [info, setInfo] = useState({});
+
+  const initialUrl = "https://rickandmortyapi.com/api/character";
+
+  const fetchCharacters = (url) => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCharacters(data.results);
+        setInfo(data.info);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const onPrevius = () => {
+    fetchCharacters(info.prev);
+  };
+
+  const onNext = () => {
+    fetchCharacters(info.next);
+  };
+
+  useEffect(() => {
+    fetchCharacters(initialUrl);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="text-center">
+        <img src={logo} alt="logo" className="logo" />
+      </div>
+
+      <div className="container">
+        <Characters characters={characters} />
+
+        <Pagination
+          prev={info.prev}
+          next={info.next}
+          onPrevius={onPrevius}
+          onNext={onNext}
+        />
+      </div>
+    </>
   );
 }
 
